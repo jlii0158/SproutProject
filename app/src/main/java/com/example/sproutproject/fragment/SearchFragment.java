ec6e475b98b0361332e32ecd2768c50cb15e0b3a
Jingwei Li <jlii0158@student.monash.edu>
2020-08-19 21:46:40 +1000

    search fragment add new page and test logic

--- app/src/main/java/com/example/sproutproject/fragment/SearchFragment.java
+++ app/src/main/java/com/example/sproutproject/fragment/SearchFragment.java
@@ -1,5 +1,6 @@
 package com.example.sproutproject.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.sproutproject.CreateActivity;
 import com.example.sproutproject.R;
 
 
 public class SearchFragment extends Fragment {
 
+    private Button bt_search,bt_back,bt_collect, bt_generate;
+    private View top_view, bottom_view;
     public SearchFragment() {
         // Required empty public constructor
     }
@@ -23,6 +28,27 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        bt_search = view.findViewById(R.id.button);
+        bt_back = view.findViewById(R.id.button4);
+        top_view = view.findViewById(R.id.search_view);
+        bottom_view = view.findViewById(R.id.plant_detail_view);
+
+        bt_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                top_view.setVisibility(View.GONE);
+                bottom_view.setVisibility(View.VISIBLE);
+            }
+        });
+
+        bt_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                top_view.setVisibility(View.VISIBLE);
+                bottom_view.setVisibility(View.GONE);
+            }
+        });
 
         return view;
     }
